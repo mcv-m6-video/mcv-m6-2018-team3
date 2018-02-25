1e5460b07ebe3ca684ce4bcb2ca463d3200a04f7
Marc Górriz <gbmarc@hotmail.es>
2018-02-25 18:40:27 +0100

    week1 task1

---
+++ Week1/task1.py
@@ -0,0 +1,12 @@
+
+
+TestDirectory = '../test_results/foreground/highway/'
+GTDirectory = '../datasets/highway/reducedGT/'
+seq_range = [1201, 1400]
+
+
+#test A results
+
+#test B results
+
+#print or write final results
---
+++ Week1/utils.py
@@ -0,0 +1,11 @@
+import cv2
+
+def load_data(data_path, seq_range=None):
+    """
+    load database and return frames between seq_range with numpy arrays
+    """
+    n_frames = seq_range[1] - seq_range[0]
+
+
+
+    return
