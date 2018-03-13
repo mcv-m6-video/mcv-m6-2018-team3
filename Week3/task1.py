8ac702ae1ae7e746f6ce5664f06bd3a613463a02
willytell <guille.torres@gmail.com>
2018-03-13 20:03:08 +0100

    task1 ready to write images

--- Week3/task1.py
+++ Week3/task1.py
@@ -31,99 +31,77 @@ def write_images(X, path, head_filename):
 
     return
 
-#INPUT: X: original images, Y: images tranformed
-# def show_images(X, Y, delay=0):
-#     #assert (X.shape[0] == Y.shape[0])
-#
-#     if (X is None) and (Y is None):
-#         print("There is no images to show.")
-#
-#     for i in range(Y.shape[0]):
-#         cv2.imshow("Original Image", X[i])
-#         cv2.imshow("Threshold Image", Y[i]*255)
-#         cv2.waitKey(delay=delay)
 
+def main():
+    show = True
+    write = True
 
-# def main():
-#     show = True
-#     write = True
-#
-#     data_path = '../../databases'
-#     PlotsDirectory = '../plots/Week3/task1/'
-#
-#     if not os.path.exists(PlotsDirectory):
-#         os.makedirs(PlotsDirectory)
-#
-#     names = ['highway', 'fall', 'traffic']
-#     estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
-#     prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
-#
-#     params = { 'highway': {'alpha': 7.25, 'rho': 0.6},
-#                'fall': {'alpha': 3.2, 'rho': 0.004},
-#                'traffic': {'alpha': 0.0, 'rho': 10.67}}
-#
-#     for i in range(len(names)):
-#         [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
-#         [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
-#
-#         if i == 0:
-#             result = task1(X_est, X_pred, params['highway']['rho'], params['highway']['alpha'])
-#         elif i==1:
-#             result = task1(X_est, X_pred, params['fall']['rho'], params['fall']['alpha'])
-#         else:
-#             result = task1(X_est, X_pred, params['traffic']['rho'], params['traffic']['alpha'])
-#
-#     # if show:
-#     #     show_images((X_est, result), 0)
-#
-#     if write:
-#         write_images(result, '~/','result')
+    data_path = '../../databases'
+    PlotsDirectory = '../plots/Week3/task1/'
 
+    if not os.path.exists(PlotsDirectory):
+        os.makedirs(PlotsDirectory)
 
-#if __name__ == "__main__":
-#    main()
+    names = ['highway', 'fall', 'traffic']
+    estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
+    prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
+
+    params = {'highway': {'alpha': 7.25, 'rho': 0.6}, 'fall': {'alpha': 3.2, 'rho': 0.004}, 'traffic': {'alpha': 0.0, 'rho': 10.67}}
+
+    for i in range(len(names)):
+        [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
+        [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
+
+        if i == 0:
+            result = task1(X_est, X_pred, params['highway']['rho'], params['highway']['alpha'])
+            if write:
+                write_images(result, PlotsDirectory, 'highway_result_')
+        elif i == 1:
+            result = task1(X_est, X_pred, params['fall']['rho'], params['fall']['alpha'])
+            if write:
+                write_images(result, PlotsDirectory, 'fall_result_')
+        else:
+            result = task1(X_est, X_pred, params['traffic']['rho'], params['traffic']['alpha'])
+        if write:
+            write_images(result, PlotsDirectory, 'traffic_result_')
+
+if __name__ == "__main__":
+    main()
 
 
 
 # ================== TESTING ================
-#im = hole_filling(images=X_pred, visualize=True)    # Manual sequence: press "Enter" to advance in the sequence
-#hole_filling2(images=X_pred, connectivity=8, visualize=True)  # Manual sequence: press "Enter" to advance in the sequence
+# show = True
+# write = True
+#
+# data_path = '../../databases'
+# PlotsDirectory = '../plots/Week3/task1/'
+#
+# if not os.path.exists(PlotsDirectory):
+#     os.makedirs(PlotsDirectory)
+#
+# names = ['highway', 'fall', 'traffic']
+# estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
+# prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
+#
+# params = {'highway': {'alpha': 7.25, 'rho': 0.6}, 'fall': {'alpha': 3.2, 'rho': 0.004}, 'traffic': {'alpha': 0.0, 'rho': 10.67}}
+#
+# for i in range(len(names)):
+#     [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
+#     [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
+#
+#     if i == 0:
+#         result = task1(X_est, X_pred, params['highway']['rho'], params['highway']['alpha'])
+#         if write:
+#             write_images(result, PlotsDirectory, 'highway_result_')
+#     elif i == 1:
+#         result = task1(X_est, X_pred, params['fall']['rho'], params['fall']['alpha'])
+#         if write:
+#             write_images(result, PlotsDirectory, 'fall_result_')
+#     else:
+#         result = task1(X_est, X_pred, params['traffic']['rho'], params['traffic']['alpha'])
+#     if write:
+#         write_images(result, PlotsDirectory, 'traffic_result_')
 
-
-show = True
-write = True
-
-data_path = '../../databases'
-PlotsDirectory = '../plots/Week3/task1/'
-
-if not os.path.exists(PlotsDirectory):
-    os.makedirs(PlotsDirectory)
-
-names = ['highway', 'fall', 'traffic']
-estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
-prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
-
-params = {'highway': {'alpha': 7.25, 'rho': 0.6}, 'fall': {'alpha': 3.2, 'rho': 0.004}, 'traffic': {'alpha': 0.0, 'rho': 10.67}}
-
-for i in range(len(names)):
-    [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
-    [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
-
-    if i == 0:
-        result = task1(X_est, X_pred, params['highway']['rho'], params['highway']['alpha'])
-        if write:
-            write_images(result, './', 'highway_result_')
-    elif i == 1:
-        result = task1(X_est, X_pred, params['fall']['rho'], params['fall']['alpha'])
-        if write:
-            write_images(result, './', 'fall_result_')
-    else:
-        result = task1(X_est, X_pred, params['traffic']['rho'], params['traffic']['alpha'])
-    if write:
-        write_images(result, './', 'traffic_result_')
-
-
-# if show:
-#      show_images(X_est, result, 0)
 
 
