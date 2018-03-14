0094378ebc84c2b477b5615625741afa50cf04eb
ivancaminal <ivancaminal72@gmail.com>
2018-03-14 05:09:30 +0100

    modification Task5

--- Week3/task5.py
+++ Week3/task5.py
@@ -1,88 +1,119 @@
 import sys
 import matplotlib.pyplot as plt
-from estimator_adaptative import evaluate
+from estimator_adaptative import evaluate, EstimatorAdaptative
 from sklearn import metrics
 from utils import *
 from task3 import task3
+from task1 import task1
+from task2 import task2
 
 data_path = '../../databases'
-PlotsDirectory = '../plots/Week3/task3/'
+PlotsDirectory = '../plots/Week3/task5/'
 
 Pr_A, Re_A, Pr_B, Re_B = list(), list(), list(), list()
-FPR_A, TPR_A, FPR_B, TPR_B = list(), list(), list(), list()
+Pr_w2, Re_w2, Pr_h4, Re_h4, Pr_t2, Re_t2 = list(), list(), list(), list(), list(), list()
 names = ['highway', 'fall', 'traffic']
 estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
 prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
-a = [{'min':0, 'max':40, 'step':1}, {'min':0, 'max':40, 'step':1},{'min':0, 'max':40, 'step':1}]
+a = [{'min':0, 'max':2, 'step':1}, {'min':0, 'max':40, 'step':1},{'min':0, 'max':40, 'step':1}]
+pixels = [4, 16, 5]
 rho = [0.599, 0.004,0]
 
 #Modify this option if you want to compute ROC or PR curves
-doROC = False
+doComputation = True
+
+if doComputation:
+    for i in range(len(names)):
+        if len(sys.argv) > 1:
+            if len(sys.argv) == 2:
+                i = names.index(str(sys.argv[1]))
+
+        print('computing ' + names[i] +' ...')
+
+        [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
+        [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
+
+        alpha_range = np.arange(a[i].get('min'), a[i].get('max'), a[i].get('step'))
+
+        for idx, alpha in enumerate(alpha_range):
+            print(str(idx) + "/" + str(len(alpha_range)) + " " + str(alpha))
+            estP_w2 = EstimatorAdaptative(alpha=alpha, rho=rho[i], metric="precision")
+            estR_w2 = EstimatorAdaptative(alpha=alpha, rho=rho[i], metric="recall")
+            estP_w2.fit(X_est)
+            estR_w2.fit(X_est)
+            X_res_A = task3(X_est, X_pred, rho[i], alpha, True)
+            X_res_B = task3(X_est, X_pred, rho[i], alpha, False)
+            X_res_h4,_ = task1(X_est, X_pred, rho[i], alpha, connectivity=4)
+            X_res_t2 = task2(X_est, X_pred, rho, alpha, pixels[i])
 
 
-for i in range(len(names)):
-    if len(sys.argv) > 1:
-        if len(sys.argv) == 2:
-            i = names.index(str(sys.argv[1]))
-
-    print('computing ' + names[i] +' ...')
-
-    [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
-    [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
-
-    alpha_range = np.arange(a[i].get('min'), a[i].get('max'), a[i].get('step'))
-
-    for idx, alpha in enumerate(alpha_range):
-        print(str(idx) + "/" + str(len(alpha_range)) + " " + str(alpha))
-        X_res_A = task3(X_est, X_pred, rho[i], alpha, True)
-        X_res_B = task3(X_est, X_pred, rho[i], alpha, False)
-
-        if doROC:
-            FPR_A.append(evaluate(X_res_A, y_pred, "fpr"))
-            TPR_A.append(evaluate(X_res_A, y_pred, "tpr"))
-            FPR_B.append(evaluate(X_res_B, y_pred, "fpr"))
-            TPR_B.append(evaluate(X_res_B, y_pred, "tpr"))
-        else:
+            Pr_w2.append(estP_w2.score(X_pred, y_pred))
+            Re_w2.append(estR_w2.score(X_pred, y_pred))
+            Pr_h4.append(evaluate(X_res_h4, y_pred, "precision"))
+            Re_h4.append(evaluate(X_res_h4, y_pred, "recall"))
             Pr_A.append(evaluate(X_res_A, y_pred, "precision"))
             Re_A.append(evaluate(X_res_A, y_pred, "recall"))
             Pr_B.append(evaluate(X_res_B, y_pred, "precision"))
             Re_B.append(evaluate(X_res_B, y_pred, "recall"))
+            Pr_t2.append(evaluate(X_res_t2, y_pred, "precision"))
+            Re_t2.append(evaluate(X_res_t2, y_pred, "recall"))
 
+        np.save(PlotsDirectory + 'Pr_w2.npy', Pr_w2)
+        np.save(PlotsDirectory + 'Re_w2.npy', Re_w2)
+        np.save(PlotsDirectory + 'Pr_h4.npy', Pr_h4)
+        np.save(PlotsDirectory + 'Re_h4.npy', Re_h4)
+        np.save(PlotsDirectory + 'Pr_A.npy', Pr_A)
+        np.save(PlotsDirectory + 'Re_A.npy', Re_A)
+        np.save(PlotsDirectory + 'Pr_B.npy', Pr_B)
+        np.save(PlotsDirectory + 'Re_B.npy', Re_B)
+        np.save(PlotsDirectory + 'Pr_t2.npy', Pr_t2)
+        np.save(PlotsDirectory + 'Re_t2.npy', Re_t2)
 
-    plt.figure()
-
-    if doROC:
-        line4, = plt.plot(np.array(FPR_A), np.array(TPR_A), 'b', label='4-connectivity AUC = ' + str(round(metrics.auc(FPR_A, TPR_A, False), 4)))
-        line8, = plt.plot(np.array(FPR_B), np.array(TPR_B), 'r', label='8-connectivity AUC = ' + str(round(metrics.auc(FPR_B, TPR_B, False), 4)))
-        plt.title("ROC curve " + names[i] + " sequence]")
-        plt.xlabel("FPR")
-        plt.ylabel("TPR")
-        # Empty lists
-        FPR_A[:] = []
-        TPR_A[:] = []
-        FPR_B[:] = []
-        TPR_B[:] = []
-    else:
-        line4, = plt.plot(np.array(Re_A), np.array(Pr_A), 'b', label='4-connectivity AUC = ' + str(round(metrics.auc(Re_A, Pr_A, True), 4)))
-        line8, = plt.plot(np.array(Re_B), np.array(Pr_B), 'r', label='8-connectivity AUC = ' + str(round(metrics.auc(Re_B, Pr_B, True), 4)))
-        plt.title("Precision vs Recall curve " + names[i] + " sequence]")
-        plt.xlabel("Recall")
-        plt.ylabel("Precision")
         # Empty lists
         Pr_A[:] = []
         Re_A[:] = []
         Pr_B[:] = []
         Re_B[:] = []
+        Pr_w2[:] = []
+        Re_w2[:] = []
+        Pr_h4[:] = []
+        Re_h4[:] = []
+        Pr_t2[:] = []
+        Re_t2[:] = []
 
-    plt.legend(handles=[line4,line8], loc='upper center', bbox_to_anchor=(0.5,-0.1))
+        if len(sys.argv) > 1:
+            break
 
-    if doROC:
-        plt.savefig(PlotsDirectory+ names[i]+'_ROCcurve_AUC.png', bbox_inches='tight')
-    else:
-        plt.savefig(PlotsDirectory + names[i] + '_PRcurve_AUC.png', bbox_inches='tight')
-    plt.close()
+else:
+    Pr_w2 = np.load(PlotsDirectory + 'Pr_w2.npy')
+    Re_w2 = np.load(PlotsDirectory + 'Re_w2.npy')
+    Pr_h4 = np.load(PlotsDirectory + 'Pr_h4.npy')
+    Re_h4 = np.load(PlotsDirectory + 'Re_h4.npy')
+    Pr_A = np.load(PlotsDirectory + 'Pr_A.npy')
+    Re_A = np.load(PlotsDirectory + 'Re_A.npy')
+    Pr_B = np.load(PlotsDirectory + 'Pr_B.npy')
+    Re_B = np.load(PlotsDirectory + 'Re_B.npy')
+    Pr_t2 = np.load(PlotsDirectory + 'Pr_t2.npy')
+    Re_t2 = np.load(PlotsDirectory + 'Re_t2.npy')
 
-    if len(sys.argv) > 1:
-        break
+    for i in range(len(names)):
+        if len(sys.argv) > 1:
+            if len(sys.argv) == 2:
+                i = names.index(str(sys.argv[1]))
+
+        print('computing ' + names[i] + ' ...')
+        plt.figure()
+        line4, = plt.plot(np.array(Re_w2), np.array(Pr_w2), 'b', label='4-connectivity AUC = ' + str(round(metrics.auc(Re_w2, Pr_w2, True), 4)))
+        line8, = plt.plot(np.array(Re_h4), np.array(Pr_h4), 'r', label='8-connectivity AUC = ' + str(round(metrics.auc(Re_h4, Pr_h4, True), 4)))
+        plt.title("Precision vs Recall curve " + names[i] + " sequence]")
+        plt.xlabel("Recall")
+        plt.ylabel("Precision")
+
+
+        plt.legend(handles=[line4,line8], loc='upper center', bbox_to_anchor=(0.5,-0.1))
+
+        plt.close()
+
+
 
 
