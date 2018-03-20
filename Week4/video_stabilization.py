ea86cc09b41e2e71c63daa4c4d4d096305bf4a3b
willytell <guille.torres@gmail.com>
2018-03-20 21:14:10 +0100

    stabilization video for everyone

--- Week4/video_stabilization.py
+++ Week4/video_stabilization.py
@@ -4,11 +4,27 @@ import numpy as np
 from block_matching import get_block_matching
 
 
-def video_stabilization(sequence):
+def get_stabilization(curr_img, prev_img, motion)
 
-    for id in len(sequence)/2:
-        perv_img
-        curr_img
+# Backward precision
+def video_stabilization(sequence, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward'):
+
+
+    N = len(sequence)+1
+
+    prev_img = sequence[0]
+
+    sequence_stabilized = np.copy(sequence)
+
+    for idx in range(1, N):
+        curr_img = sequence[idx]
+
+        optical_flow = get_block_matching(curr_img, prev_img, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
+
+        stabilized_frame = get_stabilization(curr_img, prev_img, optical_flow)
+        prev_img = stabilized_frame
+
+        sequence_stabilized [:, :, idx] = stabilized_frame
 
 
     return  sequence_stabilized
