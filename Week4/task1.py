02230c0acaec7dfcd55675c0f323a7163b89bdbf
ivancaminal <ivancaminal72@gmail.com>
2018-03-20 20:45:44 +0100

    bug fix in plots (task1)

--- Week4/task1.py
+++ Week4/task1.py
@@ -2,9 +2,13 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+from block_matching import get_block_matching
+import os
 
 data_path = '../../databases'
 PlotsDirectory = '../plots/Week4/task1/'
+if not os.path.exists(PlotsDirectory):
+    os.makedirs(PlotsDirectory)
 
 def task1(gt, test, offset):
     gtx = (np.array(gt[:, :, 1], dtype=float) - (2 ** 15)) / 64.0
@@ -41,7 +45,7 @@ def task1(gt, test, offset):
     # plt.xlabel("MSE")
 
     m_msen = np.mean(np.mean(msen_r))
-    print("x2mean square error (non-ocluded): " + str(m_msen))
+    print("MMSE (non-ocluded): " + str(m_msen))
 
     # plt.figure(2)
     # plt.imshow(msen)
@@ -53,48 +57,50 @@ def task1(gt, test, offset):
 
     # Percentage of Erroneous Pixels in Non-occluded areas
     pepn = np.count_nonzero(mask1[mask2]) / np.count_nonzero(mask1)
-    print("percentage of erroneous pixels (non-ocluded): " + str(pepn) + "\n")
+    print("PEPN (non-ocluded): " + str(pepn) + "\n")
 
 
     return m_msen, pepn
 
 def gunner_farneback(prvs, act):
-
-    hsv = np.zeros_like(prvs)
-    hsv[..., 1] = 255
-
-    flow = cv2.calcOpticalFlowFarneback(prvs, act, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    cv2.imshow('frame2', bgr)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    elif k == ord('s'):
-        cv2.imwrite('opticalfb.png', frame2)
-        cv2.imwrite('opticalhsv.png', bgr)
-    cap.release()
-    cv2.destroyAllWindows()
-    return opt_flow
+    #
+    # hsv = np.zeros_like(prvs)
+    # hsv[..., 1] = 255
+    #
+    # flow = cv2.calcOpticalFlowFarneback(prvs, act, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    # mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    # hsv[..., 0] = ang * 180 / np.pi / 2
+    # hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    # bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    # cv2.imshow('frame2', bgr)
+    # k = cv2.waitKey(30) & 0xff
+    # if k == 27:
+    #     break
+    # elif k == ord('s'):
+    #     cv2.imwrite('opticalfb.png', frame2)
+    #     cv2.imwrite('opticalhsv.png', bgr)
+    # cap.release()
+    # cv2.destroyAllWindows()
+    # return opt_flow
+    return 1
 
 def evaluate_custom(seq, dataset, names):
-    # area_offsets = np.arange(1, 65, 7)
-    # block_dims = np.arange(2, 33, 6)
-    area_offsets = [32]
-    block_dims = [8]
+    # area_offsets = np.arange(8, 8*5+1, 8)
+    # block_dims = np.arange(20, 61, 10)
+    area_offsets = np.arange(8, 8 * 6 + 1, 8)
+    block_dims = np.arange(20, 10 * 6 + 1, 10)
 
     gt = cv2.imread(names[0], -1)
     prvs = cv2.imread(names[1], 0)
-    next = cv2.imread(names[2], 0)
-    block_list_p, block_list_m, list_m, list_p = [], [], [], []
+    curr = cv2.imread(names[2], 0)
+    block_list_p, block_list_m = [], []
     combinations = len(block_dims)*len(area_offsets)
 
     for i, block in enumerate(block_dims):
+        list_m, list_p = [], []
         for j, offset in enumerate(area_offsets):
-            print("Computing "+str(i+j)+"/"+str(combinations))
-            opt_flow = # Todo: Receive gt[x,y,z] block matching from test image
+            print("Computing "+str((i*(len(area_offsets))+(j+1)))+"/"+str(combinations))
+            opt_flow = get_block_matching(curr, prvs, block, block, offset, offset)
             m_msen, pepn = task1(gt, opt_flow, offset)
             list_m.append(m_msen)
             list_p.append(pepn)
@@ -108,7 +114,7 @@ def evaluate_custom(seq, dataset, names):
     plotlines(block_list_p, area_offsets, block_dims, "PEPN", seq, dataset)
 
     # Find best mmse
-    best_mmse = best_evaluation(block_list_m, "maximum")
+    best_mmse = best_evaluation(block_list_m, "minimum")
 
     # Find best pepn
     best_pepn = best_evaluation(block_list_p, "minimum")
@@ -128,7 +134,7 @@ def evaluate_gunner_farneback(seq, dataset, names):
         for j, offset in enumerate(area_offsets):
             print("Computing " + str(i + j) + "/" + str(combinations))
             opt_flow = gunner_farneback()
-            m_msen, pepn = task1(gt, opt_flow, offset)
+            m_msen, pepn = task1(gt, opt_flow, 1)
             list_m.append(m_msen)
             list_p.append(pepn)
         block_list_m.append(list_m)
@@ -164,14 +170,15 @@ def best_evaluation(evaluations, criteria):
 def plotlines(list_values, area_offsets, blocks, title, seq, dataset):
     plt.figure()
     lines = []
-    for lv, block in zip(list_values, blocks):
-        line, = plt.plot(lv, area_offsets, 'k', label=' = ' + str(block))
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    for idx, lv, block in zip(range(len(blocks)),list_values, blocks):
+        line, = plt.plot(area_offsets, lv, colors[idx], label=' = block dimension: ' + str(block))
         lines.append(line)
     plt.title(title+" " + seq + " sequence]")
     plt.xlabel("Area of search")
     plt.ylabel(title)
     plt.legend(handles=lines, loc='upper center', bbox_to_anchor=(0.5, -0.1))
-    plt.savefig(PlotsDirectory + dataset + '_sequence' + seq + '.png', bbox_inches='tight')
+    plt.savefig(PlotsDirectory + dataset + '_sequence' + seq +" "+ title +'.png', bbox_inches='tight')
     plt.close()
 
     return
@@ -190,4 +197,7 @@ if __name__ == "__main__":
     names157 = [gt157_name, test157_name1, test157_name2]
 
     best_mmse, best_pepn = evaluate_custom("45", "KITTI", names157)
-    best_mmse, best_pepn = evaluate_gunner_farneback("45", "KITTI", names157)
+    print("Best MMSE: "+ str(best_mmse))
+    print("Best PEPN: " + str(best_pepn))
+
+    #best_mmse, best_pepn = evaluate_gunner_farneback("45", "KITTI", names157)
