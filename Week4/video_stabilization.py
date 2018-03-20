90ff5c5fc7bb1938533edfe3e20b16b1f52dfabe
willytell <guille.torres@gmail.com>
2018-03-20 20:39:23 +0100

    stabilization video 2

---
+++ Week4/video_stabilization.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+from block_matching import get_block_matching
+
+
+def video_stabilization(sequence):
+
+    for id in len(sequence)/2:
+        perv_img
+        curr_img
+
+
+    return  sequence_stabilized
