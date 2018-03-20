4dc1304358be810679ff4b8ede50ccec28315405
willytell <guille.torres@gmail.com>
2018-03-20 21:26:32 +0100

    test stabilizado!

--- Week4/video_stabilization.py
+++ Week4/video_stabilization.py
@@ -5,14 +5,7 @@ from block_matching import get_block_matching
 
 
 def get_stabilization(prev_img, motion):
-    """
-    
-    :param curr_img: grayscale frame t+1
-    :param prev_img: grayscale frame t
-    :param motion: optical_flow map, [motion_x, motion_y, valid]
-    :return: stabilized_prev_img
-    """
-    
+
     stabilized_prev_img = np.zeros(prev_img.shape)
     
     for idx in range(prev_img.shape[0]):
@@ -40,10 +33,42 @@ def video_stabilization(sequence, block_size_x, block_size_y, search_area_x, sea
 
         optical_flow = get_block_matching(curr_img, prev_img, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
 
-        stabilized_frame = get_stabilization(curr_img, prev_img, optical_flow)
+        stabilized_frame = get_stabilization(prev_img, optical_flow)
         prev_img = stabilized_frame
 
-        sequence_stabilized [:, :, idx] = stabilized_frame
+        sequence_stabilized [:, :, idx-1] = stabilized_frame
 
 
     return  sequence_stabilized
+
+
+def write_video(sequence, file_name):
+
+    H, W, C, N = sequence.shape
+    video = cv2.VideoWriter(file_name, -1, 20.0, (W, H))
+    for i in range(N):
+        frame = sequence[:,:,:,i]
+        # Video writte
+        video.write(frame)
+
+
+# =================================
+import os
+from utils import load_data
+
+data_path = '../../databases'
+PlotsDirectory = '../plots/Week3/task1/'
+
+if not os.path.exists(PlotsDirectory):
+    os.makedirs(PlotsDirectory)
+
+name = 'traffic'
+seq_range = np.array([950, 1050])
+
+
+[seq, y] = load_data(data_path, name, seq_range, grayscale=True)
+
+block_size_x, block_size_y, search_area_x, search_area_y = 20, 20, 10, 10
+est_seq = video_stabilization(seq, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
+
+write_video(est_seq, "./")
