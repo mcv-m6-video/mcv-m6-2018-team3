fb0ad0325203eb62a73699064003c264d407b51a
Marc Górriz Blanch <gbmarc@hotmail.es>
2018-03-20 21:15:39 +0100

    Update video_stabilization.py

--- Week4/video_stabilization.py
+++ Week4/video_stabilization.py
@@ -4,9 +4,28 @@ import numpy as np
 from block_matching import get_block_matching
 
 
-def get_stabilization(curr_img, prev_img, motion)
+def get_stabilization(prev_img, motion):
+    """
+    
+    :param curr_img: grayscale frame t+1
+    :param prev_img: grayscale frame t
+    :param motion: optical_flow map, [motion_x, motion_y, valid]
+    :return: stabilized_prev_img
+    """
+    
+    stabilized_prev_img = np.zeros(prev_img.shape)
+    
+    for idx in range(prev_img.shape[0]):
+        for idy in range(prev_img.shape[1]):
+            
+            if motion[idx, idy, 2] is not 0:
+                stabilized_prev_img[idx, idy] = prev_img[idx + motion[idx, idy, 0], idy + motion[idx, idy, 1]]
+    
+    return stabilized_prev_img
 
-# Backward precision
+
+
+# Backward prediction
 def video_stabilization(sequence, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward'):
 
 
