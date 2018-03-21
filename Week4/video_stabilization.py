189083d777500946a510f3f98b6d7485207775ca
willytell <guille.torres@gmail.com>
2018-03-21 12:16:03 +0100

    updated to gt

--- Week4/video_stabilization.py
+++ Week4/video_stabilization.py
@@ -19,38 +19,44 @@ from block_matching import get_block_matching
 
 
 # Backward prediction
-def video_stabilization(sequence, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward'):
+def video_stabilization(sequence, gt, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward'):
 
 
-    N = len(sequence)+1
+    N = len(sequence)
 
     prev_img = sequence[0]
 
     sequence_stabilized = np.copy(sequence)
     sequence_stabilized = np.zeros(sequence.shape)
 
+    sequence_stabilized_gt = np.copy(gt) # Only for GT
+    sequence_stabilized_gt = np.zeros(gt.shape) # Only for GT
+
     for idx in range(1, N):
         print(idx, N)
         curr_img = sequence[idx]
+        curr_img_gt = gt[idx]   # Only for GT
 
         optical_flow = get_block_matching(curr_img, prev_img, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
 
-        u = np.median(optical_flow[:,:,0])
-        v = np.median(optical_flow[:,:,1])
+        u = np.mean(optical_flow[:,:,0])
+        v = np.mean(optical_flow[:,:,1])
 
         # translation matrix
         affine_H = np.float32([[1, 0, -u],
                                [0, 1, -v]])
 
         stabilized_frame = cv2.warpAffine(curr_img, affine_H, (curr_img.shape[1], curr_img.shape[0]))
+        stabilized_frame_gt = cv2.warpAffine(curr_img_gt, affine_H, (curr_img_gt.shape[1], curr_img_gt.shape[0]))  # Only for GT
 
         # update the previous image to the estabilized current image
         prev_img = stabilized_frame
 
         sequence_stabilized [idx-1, :, :] = stabilized_frame
+        sequence_stabilized_gt[idx - 1, :, :] = stabilized_frame_gt # Only for GT
 
 
-    return  sequence_stabilized
+    return  sequence_stabilized, sequence_stabilized_gt
 
 
 # =================================
@@ -72,10 +78,12 @@ seq_range = np.array([950, 1050])
 
 
 block_size_x, block_size_y, search_area_x, search_area_y = 5, 5, 10, 10
-est_seq = video_stabilization(seq, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
+est_seq, est_gt = video_stabilization(seq, y, block_size_x, block_size_y, search_area_x, search_area_y, compensation = 'backward')
 
 np.save(PlotsDirectory + 'traffic_stabilized_bloque5_area10.npy', est_seq)
+np.save(PlotsDirectory + 'traffic_gt_stabilized_bloque5_area10.npy', est_gt)
 write_images2(est_seq, PlotsDirectory, 'traffic_stabilized_bloque5_area10_')
+write_images2(est_gt, PlotsDirectory, 'traffic_gt_stabilized_bloque5_area10_')
 
 # sequence = seq
 # N = 3  # len(sequence)+1
