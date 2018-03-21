16e1b2cd495b0f3179bb41ce51fd3f3f6048a56e
ivancaminal <ivancaminal72@gmail.com>
2018-03-21 14:55:47 +0100

    task2 almost finsihed \n (prepared to compute other stabilization method)

--- Week4/task2.py
+++ Week4/task2.py
@@ -66,29 +66,27 @@ if doComputation:
         [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
         [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
 
-        print(X_est.shape)
-        print(X_pred.shape)
-        print(y_est.shape)
-        print(y_pred.shape)
+        vid_o = np.load(data_path + "/traffic/other/traffic_stabilized_other.npy")
+        vid_o_gt = np.load(data_path + "/traffic/other/traffic_stabilized_other_gt.npy")
+        print(vid_o.shape)
+        print(vid_o_gt.shape)
 
-        # vid_o = video_to_frame(data_path+"/traffic/traffic_stabilized_other.mp4")
-        # vid_o_gt = video_to_frame(data_path + "/traffic/traffic_stabilized_other_gt.mp4")
-        # X_est_o = vid_o[estimation_range[i][0],estimation_range[i][1]]
-        # y_est_o = vid_o_gt[estimation_range[i][0],estimation_range[i][1]]
-        # X_pred_o = vid_o[prediction_range[i][0], prediction_range[i][1]]
-        # y_pred_o = vid_o_gt[prediction_range[i][0], prediction_range[i][1]]
+        X_est_o = vid_o[new_estimation_range[i][0]: new_estimation_range[i][1]+1]
+        y_est_o = vid_o_gt[new_estimation_range[i][0]:new_estimation_range[i][1]+1]
+        X_pred_o = vid_o[new_prediction_range[i][0]:new_prediction_range[i][1]+1]
+        y_pred_o = vid_o_gt[new_prediction_range[i][0]: new_prediction_range[i][1]+1]
 
-        vid_c = np.load(data_path + "/traffic/custom/traffic_stabilized_bloque5_area10.npy")
-        vid_c_gt = np.load(data_path + "/traffic/custom/traffic_gt_stabilized_bloque5_area10.npy")
-        X_est_c = vid_c[new_estimation_range[i][0]: new_estimation_range[i][1]]
-        y_est_c = vid_c_gt[new_estimation_range[i][0]:new_estimation_range[i][1]]
-        X_pred_c = vid_c[new_prediction_range[i][0]:new_prediction_range[i][1]]
-        y_pred_c = vid_c_gt[new_prediction_range[i][0]: new_prediction_range[i][1]]
+        print(X_est_o.shape)
+        print(X_pred_o.shape)
+        print(y_est_o.shape)
+        print(y_pred_o.shape)
 
-        X_est_o =  X_est_c
-        y_est_o = y_est_c
-        X_pred_o = X_pred_c
-        y_pred_o = y_pred_c
+        # vid_c = np.load(data_path + "/traffic/custom/traffic_stabilized_bloque5_area10.npy")
+        # vid_c_gt = np.load(data_path + "/traffic/custom/traffic_gt_stabilized_bloque5_area10.npy")
+        # X_est_c = vid_c[new_estimation_range[i][0]: new_estimation_range[i][1]+1]
+        # y_est_c = vid_c_gt[new_estimation_range[i][0]:new_estimation_range[i][1]+1]
+        # X_pred_c = vid_c[new_prediction_range[i][0]:new_prediction_range[i][1]+1]
+        # y_pred_c = vid_c_gt[new_prediction_range[i][0]: new_prediction_range[i][1]+1]
 
         alpha_range = np.arange(a[i].get('min'), a[i].get('max'), a[i].get('step'))
 
@@ -96,29 +94,29 @@ if doComputation:
             print(str(idx) + "/" + str(len(alpha_range)) + " " + str(alpha))
             X_res_t2 = w3task2(X_est, X_pred, rho[i], alpha, pixels[i])
             X_res_o = w3task2(X_est_o, X_pred_o, rho[i], alpha, pixels[i])
-            X_res_c = w3task2(X_est_c, X_pred_c, rho[i], alpha, pixels[i])
+            # X_res_c = w3task2(X_est_c, X_pred_c, rho[i], alpha, pixels[i])
 
             Pr_t2.append(evaluate(X_res_t2, y_pred, "precision"))
             Re_t2.append(evaluate(X_res_t2, y_pred, "recall"))
             Pr_o.append(evaluate(X_res_o, y_pred, "precision"))
             Re_o.append(evaluate(X_res_o, y_pred, "recall"))
-            Pr_c.append(evaluate(X_res_c, y_pred, "precision"))
-            Re_c.append(evaluate(X_res_c, y_pred, "recall"))
+            # Pr_c.append(evaluate(X_res_c, y_pred, "precision"))
+            # Re_c.append(evaluate(X_res_c, y_pred, "recall"))
 
         np.save(PlotsDirectory + names[i] +'_Pr_t2.npy', Pr_t2)
         np.save(PlotsDirectory + names[i] +'_Re_t2.npy', Re_t2)
         np.save(PlotsDirectory + names[i] + '_Pr_o.npy', Pr_o)
         np.save(PlotsDirectory + names[i] + '_Re_o.npy', Re_o)
-        np.save(PlotsDirectory + names[i] + '_Pr_c.npy', Pr_c)
-        np.save(PlotsDirectory + names[i] + '_Re_c.npy', Re_c)
+        # np.save(PlotsDirectory + names[i] + '_Pr_c.npy', Pr_c)
+        # np.save(PlotsDirectory + names[i] + '_Re_c.npy', Re_c)
 
         # Empty lists
         Pr_t2[:] = []
         Re_t2[:] = []
         Pr_o[:] = []
         Re_o[:] = []
-        Pr_c[:] = []
-        Re_c[:] = []
+        # Pr_c[:] = []
+        # Re_c[:] = []
 
         if len(sys.argv) > 1:
             break
@@ -133,12 +131,12 @@ else:
 
         Pr_t2 = np.load(PlotsDirectory + names[i] +'_Pr_t2.npy')
         Re_t2 = np.load(PlotsDirectory + names[i] +'_Re_t2.npy')
-        Pr_c = np.load(PlotsDirectory + names[i] + '_Pr_c.npy')
-        Re_c = np.load(PlotsDirectory + names[i] + '_Re_c.npy')
+        # Pr_c = np.load(PlotsDirectory + names[i] + '_Pr_c.npy')
+        # Re_c = np.load(PlotsDirectory + names[i] + '_Re_c.npy')
         Pr_o = np.load(PlotsDirectory + names[i] + '_Pr_o.npy')
         Re_o = np.load(PlotsDirectory + names[i] + '_Re_o.npy')
 
-        filled_plot(Pr_t2, Re_t2, Pr_c, Re_c, 'best week3', 'custom stablilization', PlotsDirectory, names[i])
+        # filled_plot(Pr_t2, Re_t2, Pr_c, Re_c, 'best week3', 'custom stablilization', PlotsDirectory, names[i])
         filled_plot(Pr_t2, Re_t2, Pr_o, Re_o, 'best week3', 'other stablilization', PlotsDirectory, names[i])
 
 
