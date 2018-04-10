06ea1fab5e19009905b91472dc7b7036dd729a3c
ivancaminal <ivancaminal72@gmail.com>
2018-04-10 12:12:52 +0200

    Visualize correction of Adaptative estimator

--- Week4/backup_week2/estimator_adaptative.py
+++ Week4/backup_week2/estimator_adaptative.py
@@ -15,27 +15,25 @@ class EstimatorAdaptative(Estimator):
     def fit(self, x, y=None):
         if y is not None:
             y = simplify_labels(y)
-            mu = np.nanmean(x * y, axis=0)
-            var = np.nanvar(x * y, axis=0)
-        else:
-            mu = np.nanmean(x, axis=0)
-            var = np.nanvar(x, axis=0)
-
-        RHO = np.ones(x.shape[1:3])*self.rho
-        mus = []
+        RHO = np.ones(x.shape[1:3])
+        mu = np.zeros(x.shape[1:3])
         for i in range(0, x.shape[0]):
             frame = x[i, :, :]
-            out = np.abs(frame - mu) >= self.alpha * (np.sqrt(var) + 2)
-            out = out.astype(np.uint8)
+            RHO[np.where(mu != 0)] = self.rho
             mu_old = mu
-            var_old = var
-            mu = (out*mu) + ((RHO * frame) + ((1.0 - RHO) * mu)) * (1 - out)
-            mus.append(mu)
-            var = (out * var) + ((RHO * (frame - mu) ** 2) + ((1.0 - RHO) * var)) * (1 - out)
+            mu = RHO * frame + (1 - RHO) * mu
             if y is not None:
                 mu[np.where(np.isnan(y[i, :, :]))] = mu_old[np.where(np.isnan(y[i, :, :]))]
-                var[np.where(np.isnan(y[i, :, :]))] = var_old[np.where(np.isnan(y[i, :, :]))]
 
+        RHO = np.ones(x.shape[1:3])
+        var = np.zeros(x.shape[1:3])
+        for i in range(0, x.shape[0]):
+            frame = x[i, :, :]
+            RHO[np.where(var != 0)] = self.rho
+            var_old = var
+            var = RHO * (frame - mu) ** 2 + (1 - RHO) * var
+            if y is not None:
+                var[np.where(np.isnan(y[i, :, :]))] = var_old[np.where(np.isnan(y[i, :, :]))]
         self.mu = mu
         self.var = var
         return self
--- Week5/visual_best.py
+++ Week5/visual_best.py
@@ -1,16 +1,18 @@
 import sys
-import os
 import matplotlib.pyplot as plt
-import numpy as np
 from sklearn import metrics
-from utils import video_to_frame, load_data
+from utils import *
 from scipy import interpolate
 
 sys.path.append("./backup_week2")
 from backup_week2.task2 import task2 as w3task2
-from backup_week2.estimator_adaptative import evaluate, EstimatorAdaptative
+from backup_week2.estimator_adaptative import EstimatorAdaptative, evaluate
 
 def filled_plot(Pr1, Re1, Pr2, Re2, name1, name2, directory, dataset):
+    Pr1 = np.nan_to_num(Pr1)
+    Re1 = np.nan_to_num(Re1)
+    Pr2 = np.nan_to_num(Pr2)
+    Re2 = np.nan_to_num(Re2)
     max_val = np.max([np.max(Re1), np.max(Re2)])
     interp = 500
     x = np.linspace(0, max_val, interp)
@@ -35,30 +37,41 @@ def filled_plot(Pr1, Re1, Pr2, Re2, name1, name2, directory, dataset):
 
     return
 
+def filled_plot_single(Pr1, Re1, name, directory, dataset):
+    Pr1 = np.nan_to_num(Pr1)
+    Re1 = np.nan_to_num(Re1)
+    max_val = np.max([np.max(Re1)])
+    interp = 500
+    x = np.linspace(0, max_val, interp)
+    y3 = np.zeros(interp)
+    f1 = interpolate.interp1d(Re1, Pr1,bounds_error=False)
+    plt.figure()
+    line1, = plt.plot(np.array(x), f1(x), 'k',
+                      label=name+' = ' + str(round(metrics.auc(Re1, Pr1, True), 4)))
+    plt.fill_between(x, y3, f1(x), where=f1(x) > y3, facecolor='black', interpolate=True)
+    plt.title("Precision vs Recall curve " + names[i] + " sequence]")
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.legend(handles=[line1], loc='upper center', bbox_to_anchor=(0.5, -0.1))
+    plt.savefig(directory + dataset +'_'+ name+'_PRcurve_AUC.png', bbox_inches='tight')
+    plt.close()
+    return
+
 data_path = '../../databases'
-PlotsDirectory = '../plots/Week4/task2/'
+PlotsDirectory = '../plots/Week5/visual/'
 if not os.path.exists(PlotsDirectory):
     os.makedirs(PlotsDirectory)
 
+Pr_w2_new, Re_w2_new, Pr_t2_new, Re_t2_new = list(), list(), list(), list()
+names = ['highway', 'fall', 'traffic']
+estimation_range = [np.array([1050, 1200]), np.array([1460, 1510]), np.array([950, 1000])]
+prediction_range = [np.array([1201, 1350]), np.array([1511, 1560]), np.array([1001, 1050])]
+a = [{'min':0, 'max':40, 'step':1}, {'min':0, 'max':40, 'step':1},{'min':0.449, 'max':40, 'step':1}]
+pixels = [4, 16, 7]
+rho = [0.0759, 0.004,0.178]
 
-Pr_t2, Re_t2 = [], []
-Pr_o, Re_o = [], []
-Pr_c, Re_c = [], []
-names = ['traffic']
-estimation_range = [np.array([950, 1000])]
-prediction_range = [np.array([1001, 1050])]
-new_estimation_range = [np.array([950 - 950, 1000 - 950])]
-new_prediction_range = [np.array([1001 - 950, 1050 - 950])]
-a = [{'min':0, 'max':40, 'step':1}]
-pixels = [5]
-rho = [0]
-
-
-#----------------------IMPORTANT---------------------------
 #Modify this option if you want to compute ROC or PR curves
 doComputation = False
-
-
 
 if doComputation:
     for i in range(len(names)):
@@ -71,50 +84,34 @@ if doComputation:
         [X_est, y_est] = load_data(data_path, names[i], estimation_range[i], grayscale=True)
         [X_pred, y_pred] = load_data(data_path, names[i], prediction_range[i], grayscale=True)
 
-        vid_o = np.load(data_path + "/traffic/other/traffic_stabilized_other.npy")
-        vid_o_gt = np.load(data_path + "/traffic/other/traffic_stabilized_other_gt.npy")
-        X_est_o = vid_o[new_estimation_range[i][0]: new_estimation_range[i][1]+1]
-        y_est_o = vid_o_gt[new_estimation_range[i][0]:new_estimation_range[i][1]+1]
-        X_pred_o = vid_o[new_prediction_range[i][0]:new_prediction_range[i][1]+1]
-        y_pred_o = vid_o_gt[new_prediction_range[i][0]: new_prediction_range[i][1]+1]
-
-
-        vid_c = np.load(data_path + "/traffic/custom/traffic_stabilized_bloque5_area10.npy")
-        vid_c_gt = np.load(data_path + "/traffic/custom/traffic_gt_stabilized_bloque5_area10.npy")
-        X_est_c = vid_c[new_estimation_range[i][0]: new_estimation_range[i][1]+1]
-        y_est_c = vid_c_gt[new_estimation_range[i][0]:new_estimation_range[i][1]+1]
-        X_pred_c = vid_c[new_prediction_range[i][0]:new_prediction_range[i][1]+1]
-        y_pred_c = vid_c_gt[new_prediction_range[i][0]: new_prediction_range[i][1]+1]
-
         alpha_range = np.arange(a[i].get('min'), a[i].get('max'), a[i].get('step'))
 
         for idx, alpha in enumerate(alpha_range):
             print(str(idx) + "/" + str(len(alpha_range)) + " " + str(alpha))
-            X_res_t2 = w3task2(X_est, X_pred, rho[i], alpha, pixels[i])
-            X_res_o = w3task2(X_est_o, X_pred_o, rho[i], alpha, pixels[i])
-            X_res_c = w3task2(X_est_c, X_pred_c, rho[i], alpha, pixels[i])
+            estP_w2_new = EstimatorAdaptative(alpha=alpha, rho=rho[i], metric="precision")
+            estR_w2_new = EstimatorAdaptative(alpha=alpha, rho=rho[i], metric="recall")
+            estP_w2_new.fit(X_est)
+            estR_w2_new.fit(X_est)
+            X_res_t2_new = w3task2(X_est, X_pred, rho[i], alpha, pixels[i], 8, 8, True)
 
-            Pr_t2.append(evaluate(X_res_t2, y_pred, "precision"))
-            Re_t2.append(evaluate(X_res_t2, y_pred, "recall"))
-            Pr_o.append(evaluate(X_res_o, y_pred_o, "precision"))
-            Re_o.append(evaluate(X_res_o, y_pred_o, "recall"))
-            Pr_c.append(evaluate(X_res_c, y_pred_c, "precision"))
-            Re_c.append(evaluate(X_res_c, y_pred_c, "recall"))
 
-        np.save(PlotsDirectory + names[i] +'_Pr_t2.npy', Pr_t2)
-        np.save(PlotsDirectory + names[i] +'_Re_t2.npy', Re_t2)
-        np.save(PlotsDirectory + names[i] + '_Pr_o.npy', Pr_o)
-        np.save(PlotsDirectory + names[i] + '_Re_o.npy', Re_o)
-        np.save(PlotsDirectory + names[i] + '_Pr_c.npy', Pr_c)
-        np.save(PlotsDirectory + names[i] + '_Re_c.npy', Re_c)
+            Pr_w2_new.append(estP_w2_new.score(y_pred, X=X_pred))
+            Re_w2_new.append(estR_w2_new.score(y_pred, X=X_pred))
+            Pr_t2_new.append(evaluate(X_res_t2_new, y_pred, "precision"))
+            Re_t2_new.append(evaluate(X_res_t2_new, y_pred, "recall"))
+
+
+        np.save(PlotsDirectory + names[i] +'_Pr_w2_new.npy', Pr_w2_new)
+        np.save(PlotsDirectory + names[i] +'_Re_w2_new.npy', Re_w2_new)
+        np.save(PlotsDirectory + names[i] +'_Pr_t2_new.npy', Pr_t2_new)
+        np.save(PlotsDirectory + names[i] +'_Re_t2_new.npy', Re_t2_new)
+
 
         # Empty lists
-        Pr_t2[:] = []
-        Re_t2[:] = []
-        Pr_o[:] = []
-        Re_o[:] = []
-        Pr_c[:] = []
-        Re_c[:] = []
+        Pr_w2_new[:] = []
+        Re_w2_new[:] = []
+        Pr_t2_new[:] = []
+        Re_t2_new[:] = []
 
         if len(sys.argv) > 1:
             break
@@ -127,18 +124,27 @@ else:
 
         print('plotting ' + names[i] + ' ...')
 
+        Pr_w2 = np.load(PlotsDirectory + names[i] +'_Pr_w2.npy')
+        Re_w2 = np.load(PlotsDirectory + names[i] +'_Re_w2.npy')
         Pr_t2 = np.load(PlotsDirectory + names[i] +'_Pr_t2.npy')
         Re_t2 = np.load(PlotsDirectory + names[i] +'_Re_t2.npy')
-        Pr_c = np.load(PlotsDirectory + names[i] + '_Pr_c.npy')
-        Re_c = np.load(PlotsDirectory + names[i] + '_Re_c.npy')
-        Pr_o = np.load(PlotsDirectory + names[i] + '_Pr_o.npy')
-        Re_o = np.load(PlotsDirectory + names[i] + '_Re_o.npy')
-
-        filled_plot(Pr_t2, Re_t2, Pr_c, Re_c, 'best week3', 'custom stablilization', PlotsDirectory, names[i])
-        filled_plot(Pr_t2, Re_t2, Pr_o, Re_o, 'best week3', 'other stablilization', PlotsDirectory, names[i])
+        Pr_w2_new = np.load(PlotsDirectory + names[i] + '_Pr_w2_new.npy')
+        Re_w2_new = np.load(PlotsDirectory + names[i] + '_Re_w2_new.npy')
+        Pr_t2_new = np.load(PlotsDirectory + names[i] + '_Pr_t2_new.npy')
+        Re_t2_new = np.load(PlotsDirectory + names[i] + '_Re_t2_new.npy')
 
 
-        # Todo Unified plot....
+        filled_plot(Pr_w2, Re_w2, Pr_t2, Re_t2, 'Adaptative', 'Best postprocesing', PlotsDirectory, names[i])
+        filled_plot_single(Pr_w2, Re_w2, 'Adaptative', PlotsDirectory, names[i])
+        filled_plot_single(Pr_t2, Re_t2, 'Best postprocesing', PlotsDirectory, names[i])
+        filled_plot(Pr_w2_new, Re_w2_new, Pr_t2_new, Re_t2_new, 'New Adaptative', 'New Best postprocesing', PlotsDirectory, names[i])
+        filled_plot_single(Pr_w2_new, Re_w2_new, 'New Adaptative', PlotsDirectory, names[i])
+        filled_plot_single(Pr_t2_new, Re_t2_new, 'New Best postprocesing', PlotsDirectory, names[i])
+        filled_plot(Pr_w2, Re_w2, Pr_w2_new, Re_w2_new, 'Adaptative', 'New Adaptative', PlotsDirectory, names[i])
+        filled_plot(Pr_t2, Re_t2, Pr_t2_new, Re_t2_new, 'Best postprocesing', 'New Best postprocesing', PlotsDirectory, names[i])
+
+
+
         # interp = 500
         # x = np.linspace(0, 1.0, interp)
         # y3 = np.zeros(interp)
@@ -152,9 +158,9 @@ else:
         # line2, = plt.plot(np.array(x), f2(x), color='#005600',
         #                   label='+holefilling' + ' = ' + str(round(metrics.auc(Re_h4, Pr_h4, True), 4)))
         # line3, = plt.plot(np.array(x), f3(x), color='#009700',
-        #                   label='+small opening' + ' = ' + str(round(metrics.auc(Re_h4, Pr_h4, True), 4)))
+        #                   label='+small opening' + ' = ' + str(round(metrics.auc(Re_B, Pr_B, True), 4)))
         # line4, = plt.plot(np.array(x), f4(x), color='#00f300',
-        #                   label='+opening' + ' = ' + str(round(metrics.auc(Re_h4, Pr_h4, True), 4)))
+        #                   label='+opening' + ' = ' + str(round(metrics.auc(Re_t2, Pr_t2, True), 4)))
         # plt.fill_between(x, f1(x), f2(x), where=f2(x) > f1(x), facecolor='#005600', interpolate=True)
         # plt.fill_between(x, f2(x), f3(x), where=f3(x) > f2(x), facecolor='#009700', interpolate=True)
         # plt.fill_between(x, f3(x), f4(x), where=f4(x) > f3(x), facecolor='#00f300', interpolate=True)
@@ -166,4 +172,3 @@ else:
         # plt.legend(handles=[line1, line2, line3, line4], loc='upper center', bbox_to_anchor=(0.5, -0.1))
         # plt.savefig(PlotsDirectory + names[i] + '_general_PRcurve_AUC.png', bbox_inches='tight')
         # plt.close()
-
